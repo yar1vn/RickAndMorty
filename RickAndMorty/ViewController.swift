8498eb57d262d12b1cf23dbb6d3ae4cf87b039ab
Yariv Nissim <yariv.nis@me.com>
2021-04-14 12:56:38 -0700

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Yariv Nissim on 4/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
